61d78a6d676453462d9e4c4ed5acc5fc86801a31
Cheng Sokdara <chengsokdara@gmail.com>
2024-04-14 20:40:47 +0700

    fixed hr

--- src/components/commons/dividers/diamond-divider.tsx
+++ src/components/commons/dividers/diamond-divider.tsx
@@ -1,6 +1,6 @@
 export default function DiamondDivider() {
   return (
-    <div className="hidden sm:flex items-center w-full ml-0 sm:ml-8">
+    <div className="hidden sm:flex items-center self-stretch ml-0 sm:ml-5">
       <div className="w-1 h-1 bg-white transform rotate-45" />
       <hr className="bg-white h-px flex-1" />
     </div>
